92fd86123af47f470c5faf1e51038e8de2a2e5d0
Zach-I-Smith <NomadLabsLLC@gmail.com>
2022-04-08 17:11:41 -0400

    Added required API functionality for managing jobs.

--- core/JomaulServer.py
+++ core/JomaulServer.py
@@ -1,5 +1,5 @@
 # Import web server utilities
-from flask import Flask, render_template, g, Blueprint
+from flask import Flask, render_template, g, Blueprint, request
 # Import jomaul utilities
 from jomaul.job import JobHandler
 # Import general utilities
@@ -20,13 +20,40 @@ if 'server_type' in config:
 
 # Define job views
 job_views = Blueprint('jobs', __name__, url_prefix='/jobs')
-@job_views.route('/')
+@job_views.route('/', methods=['GET'])
 def get_jobs():
     return job_handler.list_jobs()
 
-@job_views.route('/<job_id>')
+@job_views.route('/', methods=['POST'])
+def create_jobs():
+    configs = request.json
+    # TODO: ADD CONFIG VALIDATION
+    return job_handler.create_jobs(configs=configs)
+
+@job_views.route('/<job_id>', methods=['GET'])
 def get_job(job_id):
     return job_handler.get_job(int(job_id))
+
+@job_views.route('/<job_id>', methods=['UPDATE'])
+def update_job(job_id):
+    config = request.json
+    config['id'] = int(job_id)
+    # TODO: ADD CONFIG VALIDATION
+    return job_handler.update_job(config)
+
+@job_views.route('/<job_id>', methods=['POST'])
+def run_job(job_id):
+    content = request.json
+    parameters = None
+    if 'parameters' in content.keys():
+        parameters = content['parameters']
+    # TODO: VALIDATE PARAMETERS
+    results = job_handler.run_job(int(job_id),parameters=parameters)
+    return results
+
+@job_views.route('/<job_id>', methods=['DELETE'])
+def delete_job(job_id):
+    return job_handler.delete_job(int(job_id))
 
 app = Flask(__name__)
 if 'server_type' in config:
--- job/Job.py
+++ job/Job.py
@@ -21,6 +21,11 @@ class JobHandler:
         else:
             self.__jobs = {}
 
+    def get_owner_id(self, job_id):
+        for job in self.__jobs.keys():
+            if self.__jobs[job].get_id() == job_id:
+                return self.__jobs[job].get_owner_id()
+
     def list_jobs(self):
         return {
             "jobs":[self.__jobs[job].get_config() for job in self.__jobs.keys()]
@@ -30,6 +35,33 @@ class JobHandler:
         for job in self.__jobs.keys():
             if self.__jobs[job].get_id() == job_id:
                 return self.__jobs[job].get_config()
+
+    def update_job(self, config):
+        for job in self.__jobs.keys():
+            if self.__jobs[job].get_id() == config['id']:
+                self.__jobs[job] = Job(config = config)
+                return self.__jobs[job]
+        return "No job matching the supplied id: {}".format(config['id'])
+
+    def create_jobs(self, configs={}):
+        next_id = 0
+        for job in self.__jobs.keys():
+            if self.__jobs[job].get_id() >= next_id:
+                next_id = self.__jobs[job].get_id()
+        for new_job in configs.keys():
+            if new_job not in self.__jobs.keys():
+                configs[new_job]['id'] = next_id
+                next_id += 1
+                self.__jobs[new_job] = configs[new_job]
+            else:
+                configs[new_job] = "Job name already in use."
+        return configs
+
+    def delete_job(self, job_id):
+        for job in self.__jobs.keys():
+            if self.__jobs[job].get_id() == job_id:
+                del self.__jobs[job]
+                return {"deleted_job":job}
 
     # Saves jobs to disk
     def save_to_disk(self):
@@ -44,9 +76,12 @@ class JobHandler:
             json.dump(file_contents, job_file, indent=4)
 
     # Runs a job with the specified name, along with its dependent jobs
-    def run_job(self, job_name, parameters=None):
-        job_path = self.__jobs[job_name].get_path()
-        job = self.load_job("{}.py".format(job_path), config=self.__jobs[job_name].get_config())
+    def run_job(self, job_id, parameters=None):
+        job_path = ""
+        for job in self.__jobs.keys():
+            if self.__jobs[job].get_id() == job_id:
+                job_path = self.__jobs[job].get_path()
+        job = self.load_job("{}.py".format(job_path), config=self.__jobs[job].get_config())
         if parameters != None:
             return job.run(parameters=parameters)
         else:
@@ -77,16 +112,23 @@ class Job:
         else:
             self.__required_parameters = {}
 
+    # Get the id of the user who owns the job
+    def get_owner_id(self):
+        return self.__config['owner_id']
+
     # Returns the parameters for the targeted Job object
     def get_parameters(self):
         return self.parameters
 
+    # Get the path of the module used by the job
     def get_path(self):
         return self.__job_path
 
+    # Get the config of this specific job
     def get_config(self):
         return self.__config
 
+    # Get this specific job's id
     def get_id(self):
         return self.__config['id']
 
@@ -101,3 +143,4 @@ class Job:
                 return [False, "The following required parameter is missing: {}".format(required_parameter)]
             if self.__required_parameters[required_parameter] != None and type(self.parameters[required_parameter]) != self.__required_parameters[required_parameter]:
                 return [False, "The following parameter is a {}, when it should be a {}: {}".format(type(self.parameters[required_parameter]), self.__required_parameters[required_parameter], required_parameter)]
+        
